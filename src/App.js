90a1d6232b064b9caa46b6f74186d6078d5a2fb9
Abdul-cmd <Abdussamadahmad562@gmail.com>
2021-08-03 13:29:41 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
+import ClassTimer from './components/ClassTimer';
 import Counter from './components/Counter';
 import FocusInput from './components/FocusInput';
+import HookTimer from './components/HookTimer';
 
 
 function App() {
   return (
     <div className="App">
       <Counter /> 
-      <FocusInput />       
+      <FocusInput />
+      <ClassTimer />
+      <HookTimer />       
     </div>
   );
 }
